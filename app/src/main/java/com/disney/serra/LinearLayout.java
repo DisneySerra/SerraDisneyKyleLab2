f4f8b3c96d17ce0fd011b188276fbff8e8620a50
DisneyS <2015081509@ust-ics.mygbiz.com>
2018-09-08 23:33:30 +0800

    Initial commit

---
+++ app/src/main/java/com/disney/serra/LinearLayout.java
@@ -0,0 +1,13 @@
+package com.disney.serra;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout);
+    }
+}
